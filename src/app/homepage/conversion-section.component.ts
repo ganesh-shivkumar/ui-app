1918b2c86514d40ab1df790ac3f00b6d6996770f
ganesh-shivkumar <ganny131293@gmail.com>
2024-10-18 17:16:52 +0530

    Finalised framework

--- src/app/homepage/conversion-section.component.ts
+++ src/app/homepage/conversion-section.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-conversion-section',
   templateUrl: './conversion-section.component.html',
-  styleUrl: './conversion-section.component.scss'
+  styleUrl: './conversion-section.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConversionSectionComponent {
 
   readonly ediFormControl = new FormControl('', [Validators.required]);
   readonly jsonFormControl = new FormControl('');
+
+  details = '';
+  ranTransaction = false;
+
+  convert(){
+    this.ranTransaction = true;
+    this.details = 'Analysis is complete and no errors are found!';
+  }
+
+  openStatus(){
+    this.details = "Opens!";
+  }
 }
--- src/app/homepage/homepage.module.ts
+++ src/app/homepage/homepage.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PlaygroundComponent } from './playground.component';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { MatGridListModule} from '@angular/material/grid-list';
 import { ConversionSectionComponent } from './conversion-section.component';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
